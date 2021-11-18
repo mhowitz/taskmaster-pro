136e335e7da82352f5befd3609bf1ecdf0d91ab0
mikwitz <mikaylahowitz@gmail.com>
2021-11-18 16:24:15 -0700

    added drag and drop and remove

--- assets/js/script.js
+++ assets/js/script.js
@@ -121,6 +121,68 @@ $(".list-group").on("blur", "input[type='text']", function (){
   $(this).replaceWith(taskSpan);
 })
 
+$(".card .list-group").sortable({
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event) {
+  },
+  deactivate: function(event) {
+  },
+  over: function(event) {
+  },
+  out: function(event){
+  },
+  update: function(event) {
+    //array to store the task data in 
+    var tempArr = [];
+
+    //loop over current set of children in a sortable list
+    $(this).children().each(function() {
+      var text = $(this)
+        .find("p")
+        .text()
+        .trim();
+      var date = $(this)
+        .find("span")
+        .text()
+        .trim();
+
+      //add task dat to the temp array as an object
+      tempArr.push({
+        text: text,
+        date: date
+      })
+    });
+    //mking the trash shit droppable
+    $("#trash").droppable({
+      accept: ".card .list-group-item",
+      tolerance: "touch",
+      drop: function(event, ui){
+        ui.draggable.remove();
+      },
+      over: function(event, ui) {
+        console.log("over");
+      },
+      out: function(event, ui) {
+        console.log("out");
+      }
+    });
+    
+    
+    //trim down lists id to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+    //update array on tasks object and savve
+    tasks[arrName] = tempArr;
+    saveTasks();
+
+  }
+});
+
 
 
 
